4dacae2d68930c912712d682ebbb8d06d9980928
Citlalli Murillo <citmusa@gmail.com>
2014-09-21 21:45:56 -0500

    location setting added

--- app/src/main/java/app/sunny/citmusa/com/sunny/DetailActivity.java
+++ app/src/main/java/app/sunny/citmusa/com/sunny/DetailActivity.java
@@ -40,7 +40,7 @@ public class DetailActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+            Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
             return true;
         }
